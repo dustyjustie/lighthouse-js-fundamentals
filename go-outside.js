c376988a80676746b23d0b2deb9aa256ef145119
Justin Tsang <justin.b.tsang@gmail.com>
2016-09-20 03:07:41 +0000

    add conditionals exercises

---
+++ go-outside.js
@@ -0,0 +1,17 @@
+var temperature = -12
+
+if (temperature < 0) {
+  console.log("make sure you pick out a scarf");
+} else if (temperature < 15) {
+  console.log("short sleeves won't cut it!");
+} else {
+  console.log("short sleeves are fine.:");
+}
+
+console.log("now you're ready to go outside");
+
+var raining = true
+
+if (!raining) {
+  console.log("leave your umbrella at home!");
+}
